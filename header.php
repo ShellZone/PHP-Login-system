3a65ea5a2db9ab9a34187260298911f0d9ace48a
Roshel Samarasinghe <shellzzonez@gmail.com>
2020-04-06 16:02:15 +0100

    added styling to signup page

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 
             <ul>
             <li> <a href="index.php">Home</a> </li>
-            <li> <a href="#">Link 2</a> </li>
+            <li> <a href="#">Link 2</a> </li> 
             <li> <a href="#">Link 3</a> </li>
             <li> <a href="#">Link 4</a> </li>
             </ul>
