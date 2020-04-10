a9b87cb2d6b6ff75a38e46df13b38a066f511aea
Roshel Samarasinghe <shellzzonez@gmail.com>
2020-04-10 13:26:33 +0100

    finished error handlers

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -10,9 +10,46 @@ if (isset($_POST['signup-submit'])) {
    $password = $_POST['pwd'];
    $passwordRepeat = $_POST['pwd-repeat'];
 
+   // Checks Empty Fields
     if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
         header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
         exit();
         }
+        // Checks if email is of the right format
+        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)){
+            header("Location: ../signup.php?error=invalidmailuid&uid");
+            exit();
+        }
+        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            header("Location: ../signup.php?error=invalidmail&uid=".$username);
+        }
+        elseif (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
+            header("Location: ../signup.php?error=invalidmail&mail=".$email);
+            exit();
+        }
+        //Checks Password RePeated
+        elseif($password !== $passwordRepeat){
+            header("Location: ../signup.php?error=passwordcheck&uid=".$username."&mail=".$email);
+            exit();
+        }
+        else {
+        //   
+            $sql = "SELECT uidusers FROM users WHERE uidUsers=?";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt, $sql)){
+                header("Location: ../signup.php?error=sqlerror");
+                exit();
+            }
+            else {
+               mysqli_stmt_bind_param($stmt, "s", $username); 
+               mysqli_stmt_execute($stmt);
+               mysqli_stmt_store_result($stmt);
+               $resultCheck = mysqli_stmt_num_rows($stmt);
+               if($resultCheck > 0){
+                header("Location: ../signup.php?error=usertaken&mail=".$email);
+                exit();
+               }
+            }
 
+        }
 }
