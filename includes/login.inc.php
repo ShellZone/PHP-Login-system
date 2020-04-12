03474e89703a3212874a5052b6d13ac5740ec911
Roshel Samarasinghe <shellzzonez@gmail.com>
2020-04-12 13:56:15 +0100

    added session data stuff

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -23,12 +23,34 @@ if(empty($mailuid) || empty($password)){
     if(!mysqli_stmt_prepare($stmt, $sql)){
         header("Location: ../index.php?error=sqlerror");
         exit();
-
     }
-}
+    else {
+        mysqli_stmt_bind_param($stmt, "ss", $mailuid, $password);
+        mysqli_stmt_execute($stmt);
+        //result is equal to DB result and if its empty
+        $result = mysqli_stmt_get_result($stmt);
+        if($row = mysqli_fetch_assoc()){
+            // Hashes then checks the password
+            $pwdCheck = password_verify($password, $row['pwdUsers']);
+            if($pwdCheck == false){
+                header("Location: ../index.php?error=wrongpwd");
+                exit();
+            } elseif($pwdCheck == true){
+                //start session to create a session variable
 
-}else{
-    header("Location: ../index.php");
-    exit();
+                session_start();
+                $_SESSION['userId'] = $row['idUsers'];
+                
+            }else{
+                header("Location: ../index.php?error=wrongpwd");
+                exit();
+            }
 
+        }
+        else {
+            header("Location: ../index.php?error=nouser");
+            exit();
+        }
+    }
+  }
 }
